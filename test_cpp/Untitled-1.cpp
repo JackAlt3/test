93bea71277e6fe7cd67dc3e211f57c7c8387e0e7
Jack <your-email@example.com>
2025-01-22 19:14:38 +0530

    two

--- test_cpp/Untitled-1.cpp
+++ test_cpp/Untitled-1.cpp
@@ -5,10 +5,10 @@ typedef int gotya;
 int main() {
    
     int a = 5; 
-    int b = 25;
+    //int b = 25;
 
-    double result = (double) a /  (double) b * 100 ;  
-    std::cout << "Result " << result << std::endl;
+    //double result = (double) a /  (double) b * 100 ;  
+    //std::cout << "Result " << result << std::endl;
     
     return 0;
 }
